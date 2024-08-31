814af7ff9747b0a10f40e552fa6a42c4033c96b3
MiguelNavarrete-dev <miguel.navarrete.dev@gmail.com>
2024-08-31 13:09:16 -0400

    Delete tienda_Virtual directory
    Se elimina ya que se cambia el formato y algunos archivos

--- tienda_Virtual/src/app/interfaces/cart.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Icart{
-    IdProduct: number;
-    name: string;
-    price: number; 
-    quantity: number;
-    productImage: string;
-}
--- tienda_Virtual/src/app/interfaces/product.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Iproduct{
-    id: number;
-    name: string;
-    descripcion: string;
-    price: number;
-    img: string;
-}
-  
